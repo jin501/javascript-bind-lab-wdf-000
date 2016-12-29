3440aef6c32f1d5a13cf174a216bab1d3e614a00
jin501 <soljin01@gmail.com>
2016-12-28 22:56:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much.";
+// const app = "I don't do much.";
+
+// let digitalClock = {
+//   time: Math.floor(Date.now()/1000),
+//   startTicking: ()=> {
+//     setInterval(() => this.time ++, 1000);
+//   }
+// }
+
+var digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking() {
+    setInterval(function(){
+      this.time++
+    }.bind(this), 1000)
+  }
+}
+
+function censor(word, string) {
+  let regex = new RegExp(word, 'gi');
+  return string.replace(regex, "BLEEP");
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+
+const drugsCensor = censor.bind(null, 'drugs');
